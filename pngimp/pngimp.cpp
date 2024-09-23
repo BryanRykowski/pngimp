936158da8e3ab9c91a62c38d1ba09866e5c11285
Bryan Rykowski <90996715+BryanRykowski@users.noreply.github.com>
2024-09-23 16:14:06 -0400

    Define interface.

--- pngimp/pngimp.cpp
+++ pngimp/pngimp.cpp
@@ -1,1 +1,25 @@
 #include "pngimp.hpp"
+
+namespace pngimp
+{
+	Image::Image(std::filesystem::path path)
+	{
+
+	}
+
+	int Image::width()
+	{
+		return m_width;
+	}
+
+	int Image::height()
+	{
+		return m_height;
+	}
+
+	char* Image::data()
+	{
+		return (char*)m_data.data();
+	}
+}
+
--- pngimp/pngimp.hpp
+++ pngimp/pngimp.hpp
@@ -1,1 +1,18 @@
 #pragma once
+#include <filesystem>
+#include <vector>
+
+namespace pngimp
+{
+	class Image
+	{
+		std::vector<unsigned char> m_data;
+		int m_width = 0;
+		int m_height = 0;
+
+		Image(std::filesystem::path path);
+		int width();
+		int height();
+		char* data();
+	};
+}
